b5a41f701e87b38005cb558f89e52496aeaf57d5
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2017-05-15 15:04:14 +0700

    Savings Account

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -2,36 +2,19 @@ package eu.sig.training.ch04;
 
 // tag::SavingsAccount[]
 public class SavingsAccount {
-    private static final float INTEREST_PERCENTAGE = 0.04f;
-    private Money balance = new Money();
     private CheckingAccount registeredCounterAccount;
 
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
         return getTransfer(counterAccount, amount);
-    }
-    
-    private int getSum( String counterAccount ) {
-    	int sum = 0;
-    	
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        
-        return sum;
     }
 
 	private Transfer getTransfer(String counterAccount, Money amount) throws BusinessException {
-		int sum = getSum( counterAccount );
+		int sum = registeredCounterAccount.getSum( counterAccount );
 		
 		if (sum % 11 == 0) {
-            // 2. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
-            // 3. Check whether withdrawal is to registered counter account:
+            Transfer result = new Transfer(this, acct, amount);
             if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
             {
                 return result;
