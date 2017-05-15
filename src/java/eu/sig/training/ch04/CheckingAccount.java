6f843a7e52efb25c409bbf29c2e2ad83f79e30ac
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2017-05-15 14:40:54 +0700

    Checking Account

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -8,25 +8,31 @@ public class CheckingAccount {
 
     public Transfer makeTransfer(String counterAccount, Money amount)
         throws BusinessException {
-        // 1. Check withdrawal limit:
         if (amount.greaterThan(this.transferLimit)) {
             throw new BusinessException("Limit exceeded!");
         }
-        // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
+    	
+        int sum = getSum( counterAccount );
+        
         if (sum % 11 == 0) {
-            // 3. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount);
             return result;
         } else {
             throw new BusinessException("Invalid account number!");
         }
+    }
+    
+    public int getSum( String counterAccount ) {
+    	int sum = 0;
+    	
+        for (int i = 0; i < counterAccount.length(); i++) {
+            char character = counterAccount.charAt(i);
+            int characterValue = Character.getNumericValue(character);
+            sum = sum + (9 - i) * characterValue;
+        }
+        
+        return sum;
     }
 
     public void addInterest() {
