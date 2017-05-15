de8047d7ca7b8115c851143a2975cf2ea0b22cae
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2017-05-15 14:59:11 +0700

    Savings Account

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -9,13 +9,25 @@ public class SavingsAccount {
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
         // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
+        return getTransfer(counterAccount, amount);
+    }
+    
+    private int getSum( String counterAccount ) {
+    	int sum = 0;
+    	
         for (int i = 0; i < counterAccount.length(); i++) {
             char character = counterAccount.charAt(i);
             int characterValue = Character.getNumericValue(character);
             sum = sum + (9 - i) * characterValue;
         }
-        if (sum % 11 == 0) {
+        
+        return sum;
+    }
+
+	private Transfer getTransfer(String counterAccount, Money amount) throws BusinessException {
+		int sum = getSum( counterAccount );
+		
+		if (sum % 11 == 0) {
             // 2. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount); // <2>
@@ -29,15 +41,6 @@ public class SavingsAccount {
         } else {
             throw new BusinessException("Invalid account number!!");
         }
-    }
-
-    public void addInterest() {
-        Money interest = balance.multiply(INTEREST_PERCENTAGE);
-        if (interest.greaterThan(0)) {
-            balance.add(interest);
-        } else {
-            balance.substract(interest);
-        }
-    }
+	}
 }
 // end::SavingsAccount[]
